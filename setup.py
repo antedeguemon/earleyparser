1d56298947a994b843c7416d3cbbab068bdc55fe
antedeguemon <vammerlo@inf.ufrgs.br>
2017-11-22 15:18:03 -0200

    Setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'earleyparser',
+  packages = ['earleyparser'],
+  version = '0.1',
+  description = 'earley parser algortihm',
+  author = 'Vicente Merlo',
+  author_email = 'vammerlo@inf.ufrgs.br',
+  url = 'https://github.com/antedeguemon/earleyparser',
+  download_url = 'https://github.com/antedeguemon/earleyparser/archive/0.1.tar.gz',
+  keywords = ['context', 'grammar', 'parser', 'earley'],
+  classifiers = [],
+)
